ca6adfcac67dffffa3995abba0c34bcf88e2ee6f
Nikolaj4o <nikolay.avramov99@gmail.com>
2024-08-21 11:48:13 +0300

    update poseidon script

--- src/bin/test_poseidon.zsh
+++ src/bin/test_poseidon.zsh
@@ -11,27 +11,23 @@ mkdir params_ipa
 wait
 ./target/release/time_circuit examples/mnist/model.msgpack examples/mnist/inp.msgpack kzg true 0 15 10 halo2pngs/mnist_pos_kzg_15_10.png > halo2logs/mnist_pos_kzg_15_10.txt &
 wait
-./target/release/time_circuit examples/cifar/dlrm.msgpack examples/cifar/dlrm_input.msgpack kzg true 0 19 10 halo2pngs/dlrm_pos_kzg_19_12.png > halo2logs/dlrm_pos_kzg_19_12.txt &
-wait
-./target/release/time_circuit examples/cifar/vgg.msgpack examples/cifar/vgg_input.msgpack kzg true 0 23 10 halo2pngs/vgg_pos_kzg_23_10.png > halo2logs/vgg_pos_kzg_23_10.txt &
-wait
 
-./target/release/time_circuit examples/mnist/poseidon_model.msgpack examples/mnist/inp.msgpack kzg true 0 16 12 halo2pngs/snet_pos_kzg_16_12.png > halo2logs/snet_pos_kzg_16_12.txt &
+./target/release/time_circuit examples/mnist/poseidon_model.msgpack examples/mnist/inp.msgpack kzg true 0 16 15 halo2pngs/snet_pos_kzg_16_12.png > halo2logs/snet_pos_kzg_16_15.txt &
 wait
-./target/release/time_circuit examples/cifar/mnist_p.msgpack examples/mnist/inp.msgpack kzg true 0 15 12 halo2pngs/mnist_pos_kzg_15_12.png > halo2logs/mnist_pos_kzg_15_12.txt &
+./target/release/time_circuit examples/cifar/mnist_p.msgpack examples/mnist/inp.msgpack kzg true 0 15 15 halo2pngs/mnist_pos_kzg_15_12.png > halo2logs/mnist_pos_kzg_15_15.txt &
 wait
-./target/release/time_circuit examples/cifar/dlrm_p.msgpack examples/cifar/dlrm_input.msgpack kzg true 0 19 12 halo2pngs/dlrm_pos_kzg_19_12.png > halo2logs/dlrm_pos_kzg_19_12.txt &
+./target/release/time_circuit examples/cifar/dlrm_p.msgpack examples/cifar/dlrm_input.msgpack kzg true 0 19 15 halo2pngs/dlrm_pos_kzg_19_12.png > halo2logs/dlrm_pos_kzg_19_15.txt &
 wait
-./target/release/time_circuit examples/cifar/vgg_p.msgpack examples/cifar/vgg_input.msgpack kzg true 0 23 12 halo2pngs/vgg_pos_kzg_23_12.png > halo2logs/vgg_pos_kzg_23_12.txt &
+./target/release/time_circuit examples/cifar/vgg_p.msgpack examples/cifar/vgg_input.msgpack kzg true 0 23 15 halo2pngs/vgg_pos_kzg_23_12.png > halo2logs/vgg_pos_kzg_23_15.txt &
 
 wait
-./target/release/time_circuit examples/mnist/poseidon_model.msgpack examples/mnist/inp.msgpack kzg true 0 16 14 halo2pngs/snet_pos_kzg_16_14.png > halo2logs/snet_pos_kzg_16_14.txt &
+./target/release/time_circuit examples/mnist/poseidon_model.msgpack examples/mnist/inp.msgpack kzg true 0 16 20 halo2pngs/snet_pos_kzg_16_14.png > halo2logs/snet_pos_kzg_16_20.txt &
 wait
-./target/release/time_circuit examples/cifar/mnist_p.msgpack examples/mnist/inp.msgpack kzg true 0 15 14 halo2pngs/mnist_pos_kzg_15_14.png > halo2logs/mnist_pos_kzg_15_14.txt &
+./target/release/time_circuit examples/cifar/mnist_p.msgpack examples/mnist/inp.msgpack kzg true 0 15 20 halo2pngs/mnist_pos_kzg_15_14.png > halo2logs/mnist_pos_kzg_15_20.txt &
 wait
-./target/release/time_circuit examples/cifar/dlrm_p.msgpack examples/cifar/dlrm_input.msgpack kzg true 0 19 14 halo2pngs/dlrm_pos_kzg_19_14.png > halo2logs/dlrm_pos_kzg_19_14.txt &
+./target/release/time_circuit examples/cifar/dlrm_p.msgpack examples/cifar/dlrm_input.msgpack kzg true 0 19 20 halo2pngs/dlrm_pos_kzg_19_14.png > halo2logs/dlrm_pos_kzg_19_20.txt &
 wait
-./target/release/time_circuit examples/cifar/vgg_p.msgpack examples/cifar/vgg_input.msgpack kzg true 0 23 14 halo2pngs/vgg_pos_kzg_23_14.png > halo2logs/vgg_pos_kzg_23_14.txt &
+./target/release/time_circuit examples/cifar/vgg_p.msgpack examples/cifar/vgg_input.msgpack kzg true 0 23 20 halo2pngs/vgg_pos_kzg_23_14.png > halo2logs/vgg_pos_kzg_23_20.txt &
 wait
 # # ./target/release/time_circuit examples/cifar/diffusion.msgpack examples/cifar/diffusion_input.msgpack kzg false 2 25 13 halo2pngs/diffusion_kzg.png > halo2logs/diffusion_kzg.txt &
 # # wait
